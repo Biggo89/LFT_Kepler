e449449a621ef284b3e063dc5c54b57b42f7b4cf
Biggo <aledev.89@gmail.com>
2016-05-15 15:51:02 +0200

    aggiunta classe OpCode e Instruction

---
+++ src/Lexical_Analyzer/OpCode.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package Lexical_Analyzer;
+
+/**
+ * @author aless
+ *
+ */
+public enum OpCode {
+	ldc, imul, ineg, idiv, iadd, isub, istore, ior, iand, iload, if_icmpeq, if_icmple, if_icmplt,
+	if_icmpne, if_icmpge, if_icmpgt, ifne, GOto, invokestatic, label
+}
